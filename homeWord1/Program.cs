c1cc1e72754b6beb8c0a656a62a0fccbfac1ffb6
davud <davud.haci.2001@gmail.com>
2021-11-11 23:15:24 +0400

    first

---
+++ homeWord1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace homeWord1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(dongu));
+            Thread thread2 = new Thread(new ThreadStart(yazdir));
+            thread1.Start();
+            thread2.Start();
+
+        }
+        public static void dongu()
+        {
+            for(var i=0; i<10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void yazdir()
+        {
+            Console.WriteLine("Salam");
+            Console.WriteLine("necesen");
+        }
+    }
+}
